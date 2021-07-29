bee929a6b25a4384b9424fd2a0b63c75a46c1a51
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2021-07-29 12:23:25 -0300

    Add project files.

---
+++ AluguelCarros/Domain/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelCarros.Domain
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+        public double TotalPayment { get { return BasicPayment + Tax; } }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public override string ToString()
+        {
+            return "BASIC PAYMENT: " + BasicPayment.ToString("F2") + 
+                   "\nTAX: " + Tax.ToString("F2") +
+                   "\nTOTAL PAYMENT: " + TotalPayment.ToString("F2");
+        }
+
+    }
+}
---
+++ AluguelCarros/Program.cs
@@ -0,0 +1,35 @@
+﻿using AluguelCarros.Domain;
+using AluguelCarros.Services;
+using System;
+using System.Globalization;
+
+namespace AluguelCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine());
+
+            CarRental carRental = new(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new(hour, day, new BrazilTaxService());
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
